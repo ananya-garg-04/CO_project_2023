7d535391cefcf9a75fcab0e4e23f0eea7bc6c745
ananya-dot <83594973+ananya-dot@users.noreply.github.com>
2023-06-06 20:48:16 +0530

    Add files via upload

--- CO_A_P1/SimpleSimulator/simulator.py
+++ CO_A_P1/SimpleSimulator/simulator.py
@@ -1,6 +1,6 @@
 #import sys
 #text = sys.stdin.readlines()
-f=open("file.txt","r")
+f=open("test.txt","r")
 text=f.readlines()
 f.close()
 text = [line.strip() for line in text if line.strip()]
@@ -23,9 +23,17 @@ F = {'11010':'hlt'}
 memory=text.copy()
 for i in range(128-l):
     memory.append("0"*16)
-
+V,L,G,e = "0","0","0","0"
 program_counter=0
 halted=False
+
+def print_pc():
+    binpc = bin(program_counter)
+    binpc = str(binpc).lstrip("0b")
+    len_binpc = len(binpc)
+    len_binpc = 7 - len_binpc
+    print(len_binpc*"0"+binpc, end = " "*8)
+
 
 def print_reg_values():
     for reg in reg_val:
@@ -35,116 +43,181 @@ def print_reg_values():
         len_regval = len(str(registervalue))
         # print(len_regval)
         len_regval = 16 - len_regval
-        print(str(registervalue)+len_regval*"0")
+        print(str(registervalue)+len_regval*"0",end = " ")
+while(halted == False):
+    for i in range(l):
+        line=text[i]
+        ins=line[0:5]
+        if ins in A:
+            unused=line[5:7]
+            reg1=line[7:10]
+            reg2=line[10:13]
+            reg3=line[13::]
+            if ins=='00000':
+                reg_val[reg_encoding[reg1]]=reg_val[reg_encoding[reg2]]+reg_val[reg_encoding[reg3]]
+                print_pc()
+                program_counter+=1
+                
+                print_reg_values()
+            elif ins=='00001':
+                reg_val[reg_encoding[reg1]]=reg_val[reg_encoding[reg2]]-reg_val[reg_encoding[reg3]]
+                print_pc()
+                program_counter+=1
+                print_reg_values()
+            elif ins=='00110':
+                reg_val[reg_encoding[reg1]]=reg_val[reg_encoding[reg2]]*reg_val[reg_encoding[reg3]]
+                print_pc()
+                program_counter+=1
+                print_reg_values()
+            elif ins=='01010':
+                reg_val[reg_encoding[reg1]]=reg_val[reg_encoding[reg2]]^reg_val[reg_encoding[reg3]]
+                print_pc()
+                program_counter+=1
+                print_reg_values()
+            elif ins=='01011':
+                reg_val[reg_encoding[reg1]]=reg_val[reg_encoding[reg2]]|reg_val[reg_encoding[reg3]]
+                print_pc()
+                program_counter+=1
+                print_reg_values()
+            elif ins=='01100':
+                reg_val[reg_encoding[reg1]]=reg_val[reg_encoding[reg2]]&reg_val[reg_encoding[reg3]]
+                print_pc()
+                program_counter+=1
+                print_reg_values()
+        elif ins in B:
+            unused=line[5:6]
+            reg1=line[6:9]
+            imm=line[9::]
+            imm=int(imm)
+            decimal, i = 0, 0
+            while(imm != 0):
+                dec = imm % 10
+                decimal = decimal + dec * pow(2, i)
+                imm = imm//10
+                i += 1
+            imm=decimal
+            if ins=='00010':
+                reg_val[reg_encoding[reg1]]=imm
+                print_pc()
+                program_counter+=1
+                print_reg_values()
+            elif ins=='01000':
+                reg_val[reg_encoding[reg1]]/=(2**imm)
+                print_pc()
+                program_counter+=1
+                print_reg_values()
+            elif ins=='01001':
+                reg_val[reg_encoding[reg1]]*=(2**imm)
+                print_pc()
+                program_counter+=1
+                print_reg_values()
+        elif ins in C:
+            unused=line[5:10]
+            reg1=line[10:13]
+            reg2=line[13::]
+            if ins=='00011':
+                reg_val[reg_encoding[reg1]]=reg_val[reg_encoding[reg2]]
+                program_counter+=1
+                print_reg_values()
+            elif ins=='00111':
+                if reg_val[reg_encoding[reg2]]!=0:
+                    reg_val["R0"]=reg_val[reg_encoding[reg1]]//reg_val[reg_encoding[reg2]]
+                    reg_val["R1"]=reg_val[reg_encoding[reg1]]%reg_val[reg_encoding[reg2]]
+                if reg_val[reg_encoding[reg2]]==0:
+                    V='1'
+                    reg_val["R0"]=0
+                    reg_val["R1"]=0
+                print_reg_values()
+                print_pc()
+                program_counter+=1
+            elif ins=='01110':
+                if reg_val[reg_encoding[reg1]]<reg_val[reg_encoding[reg2]]:
+                    L='1'
+                elif reg_val[reg_encoding[reg1]]>reg_val[reg_encoding[reg2]]:
+                    G='1'
+                elif reg_val[reg_encoding[reg1]]==reg_val[reg_encoding[reg2]]:
+                    e='1'
+                print_reg_values()
+                print_pc()
+                program_counter+=1
+            elif ins=='01101':
+                reg_val[reg_encoding[reg1]]=~reg_val[reg_encoding[reg2]]
+                print_reg_values()
+                print_pc()
+                program_counter+=1
+        elif ins in D:
+            unused=line[5:6]
+            reg1=line[6:9]
+            mem=line[9::]
+            mem=int(mem)
+            decimal, i = 0, 0
+            while(mem != 0):
+                dec = mem % 10
+                decimal = decimal + dec * pow(2, i)
+                mem = mem//10
+                i += 1
+            mem=decimal
+            val=(int(memory[mem]))
+            decimal, i = 0, 0
+            while(val != 0):
+                dec = val % 10
+                decimal = decimal + dec * pow(2, i)
+                val = val//10
+                i += 1
+            val=decimal
+            if ins=='00100':
+                reg_val[reg_encoding[reg1]]=val
+                print_reg_values()
+                print_pc()
+                program_counter+=1
+            elif ins=='00101':
+                val=reg_val[reg_encoding[reg1]]
+                val=bin(val)
+                val=val.lstrip('0')
+                val=val.lstrip('b')
+                memory[mem]=val
+                print_reg_values()
+                print_pc()
+                program_counter+=1
+        elif ins in E:
+            unused=line[5:9] 
+            mem=line[9::]
+            mem=int(mem)
+            if ins == "01111":
+                program_counter = int("0b"+mem)
+                print_reg_values()
+                print_pc()
 
-for i in range(l):
-    line=text[i]
-    ins=line[0:5]
-    if ins in A:
-        unused=line[5:7]
-        reg1=line[7:10]
-        reg2=line[10:13]
-        reg3=line[13::]
-        if ins=='00000':
-            reg_val[reg_encoding[reg1]]=reg_val[reg_encoding[reg2]]+reg_val[reg_encoding[reg3]]
+            elif ins == "11100" :
+                if(L=="1"):
+                    program_counter = int("0b"+mem)
+                    print_reg_values()
+                    print_pc()
+            elif ins == "11101":
+                if(G=="1"):
+                    program_counter = int("0b"+mem)
+                    print_reg_values()
+                    print_pc()
+            elif ins == "11111":
+                if(e=="1"):
+                    program_counter = int("0b"+mem)
+                    print_reg_values()
+                    print_pc()
+
+        elif ins in F:
+            halted = True
+            print_pc()
             print_reg_values()
-        elif ins=='00001':
-            reg_val[reg_encoding[reg1]]=reg_val[reg_encoding[reg2]]-reg_val[reg_encoding[reg3]]
-            print_reg_values()
-        elif ins=='00110':
-            reg_val[reg_encoding[reg1]]=reg_val[reg_encoding[reg2]]*reg_val[reg_encoding[reg3]]
-            print_reg_values()
-        elif ins=='01010':
-            reg_val[reg_encoding[reg1]]=reg_val[reg_encoding[reg2]]^reg_val[reg_encoding[reg3]]
-            print_reg_values()
-        elif ins=='01011':
-            reg_val[reg_encoding[reg1]]=reg_val[reg_encoding[reg2]]|reg_val[reg_encoding[reg3]]
-            print_reg_values()
-        elif ins=='01100':
-            reg_val[reg_encoding[reg1]]=reg_val[reg_encoding[reg2]]&reg_val[reg_encoding[reg3]]
-            print_reg_values()
-    elif ins in B:
-        unused=line[5:6]
-        reg1=line[6:9]
-        imm=line[9::]
-        imm=int(imm)
-        decimal, i = 0, 0
-        while(imm != 0):
-            dec = imm % 10
-            decimal = decimal + dec * pow(2, i)
-            imm = imm//10
-            i += 1
-        imm=decimal
-        if ins=='00010':
-            reg_val[reg_encoding[reg1]]=imm
-            print_reg_values()
-        elif ins=='01000':
-            reg_val[reg_encoding[reg1]]/=(2**imm)
-            print_reg_values()
-        elif ins=='01001':
-            reg_val[reg_encoding[reg1]]*=(2**imm)
-            print_reg_values()
-    elif ins in C:
-        unused=line[5:10]
-        reg1=line[10:13]
-        reg2=line[13::]
-        if ins=='00011':
-            reg_val[reg_encoding[reg1]]=reg_val[reg_encoding[reg2]]
-            print_reg_values()
-        elif ins=='00111':
-            if reg_val[reg_encoding[reg2]]!=0:
-                reg_val["R0"]=reg_val[reg_encoding[reg1]]//reg_val[reg_encoding[reg2]]
-                reg_val["R1"]=reg_val[reg_encoding[reg1]]%reg_val[reg_encoding[reg2]]
-            if reg_val[reg_encoding[reg2]]==0:
-                V='1'
-                reg_val["R0"]=0
-                reg_val["R1"]=0
-            print_reg_values()
-        elif ins=='01110':
-            if reg_val[reg_encoding[reg1]]<reg_val[reg_encoding[reg2]]:
-                L='1'
-            elif reg_val[reg_encoding[reg1]]>reg_val[reg_encoding[reg2]]:
-                G='1'
-            elif reg_val[reg_encoding[reg1]]==reg_val[reg_encoding[reg2]]:
-                E='1'
-            print_reg_values()
-        elif ins=='01101':
-            reg_val[reg_encoding[reg1]]=~reg_val[reg_encoding[reg2]]
-            print_reg_values()
-    elif ins in F:
-        print_reg_values()
-        for i in memory:
-            print(i)
-        exit(0)
-    elif ins in D:
-        unused=line[5:6]
-        reg1=line[6:9]
-        mem=line[9::]
-        mem=int(mem)
-        decimal, i = 0, 0
-        while(mem != 0):
-            dec = mem % 10
-            decimal = decimal + dec * pow(2, i)
-            mem = mem//10
-            i += 1
-        mem=decimal
-        val=(int(memory[mem]))
-        decimal, i = 0, 0
-        while(val != 0):
-            dec = val % 10
-            decimal = decimal + dec * pow(2, i)
-            val = val//10
-            i += 1
-        val=decimal
-        if ins=='00100':
-            reg_val[reg_encoding[reg1]]=val
-            print_reg_values()
-        elif ins=='00101':
-            val=reg_val[reg_encoding[reg1]]
-            val=bin(val)
-            val=val.lstrip('0')
-            val=val.lstrip('b')
-            memory[mem]=val
-            print_reg_values()
-    elif ins in E:
-            
+            for i in memory:
+                print(i)
+            # print(memory)
+            # for i in range(l):
+
+            #     # print_pc()
+            #     print_reg_values()
+            # for i in memory:
+            #     # print(i)
+            exit(0)
+    
+
+
